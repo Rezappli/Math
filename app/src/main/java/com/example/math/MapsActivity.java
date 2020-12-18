efeeba413b2a6521152eea09096548c9428ba7ea
Rezappli <66002646+Rezappli@users.noreply.github.com>
2020-12-18 11:38:37 +0100

    Etape 1 à 7

--- app/src/main/java/com/example/math/MapsActivity.java
+++ app/src/main/java/com/example/math/MapsActivity.java
@@ -14,8 +14,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,7 +27,10 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,41 +40,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     public static LatLng target;
     private static List<Satellite> satelliteList;
-    private static Handler h;
-    private Runnable r;
+    private static Handler h1, h;
+    private Runnable r1, r;
     private static int speed = 1;
-    private Button speed_button;
+    private Button suivant;
+    private boolean start;
+    private int sleep = 0;
+    private TextView instructions;
+    private int etape;
+    private Satellite s, s1, s2;
+    private Polyline line;
+    private Marker m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        speed_button = findViewById(R.id.speedButton);
-        speed_button.setOnClickListener(new View.OnClickListener() {
+        instructions = findViewById(R.id.instruction);
+
+        suivant = findViewById(R.id.suivant);
+        suivant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch(speed){
-                    case 1:
-                        speed = 2;
+                switch (etape){
+                    case 1 : etape1bis();
                         break;
-                    case 2:
-                        speed = 3;
+                    case 2 : etape2();
                         break;
-                    case 3:
-                        speed = 4;
+                    case 3 : etape3();
                         break;
-                    case 4:
-                        speed = 5;
+                    case 4 : etape4();
                         break;
-                    case 5:
-                        speed = 1;
+                    case 5 : etape5();
+                        break;
+                    case 6 : etape6();
+                        break;
+                    case 7 : etape7();
                         break;
                 }
 
-                speed_button.setText("Speed " + speed);
+
+                }
+
+                //speed_button.setText("Speed " + speed);
+            });
+
+
+        suivant.setVisibility(View.INVISIBLE);
+
+        h1 = new Handler();
+        r1 = new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                if(sleep < 2){
+                    sleep++;
+                    h1.postDelayed(this,1000);
+                }else{
+                    suivant.setVisibility(View.VISIBLE);
+                }
             }
-        });
+        };
+
+        h1.postDelayed(r1,0);
+
+
+
+
 
         satelliteList = new ArrayList<>();
         h = new Handler();
@@ -80,30 +118,162 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 float[] results = new float[3];
                 if(satelliteList != null) {
                     for (Satellite s : satelliteList) {
-                        Location.distanceBetween(s.getLatLng().latitude+7+0.5*speed, s.getLatLng().longitude, target.latitude, target.longitude, results);
-                        if(results[0] < 2500000){
-                            mMap.addMarker(new MarkerOptions().title("Votre position").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-                        }else {
-                            Log.w("Satellite", "Position : " + s.getLatLng().toString());
-                            s.setLatLng(new LatLng(s.getLatLng().latitude + 0.5 * speed, s.getLatLng().longitude));
-                            Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                        if(s.getName().equalsIgnoreCase("SAT1")){
+                            Location.distanceBetween(s.getLatLng().latitude+0.1*speed, s.getLatLng().longitude, target.latitude, target.longitude, results);
+                            if(results[0] <= 2000000){
+
+                                mMap.addMarker(new MarkerOptions().title("Votre position").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                                h.removeCallbacks(r);
+                            }else {
+                                Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                                s.setLatLng(new LatLng(s.getLatLng().latitude + 0.1 * speed, s.getLatLng().longitude));
+                                Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                            }
                         }
+                        if(s.getName().equalsIgnoreCase("SAT2")){
+                            Location.distanceBetween(s.getLatLng().latitude, s.getLatLng().longitude+0.1*speed, target.latitude, target.longitude, results);
+                            if(results[0] <= 2000000){
+
+                                h.removeCallbacks(r);
+
+                            }else {
+                                Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                                s.setLatLng(new LatLng(s.getLatLng().latitude, s.getLatLng().longitude  + 0.1 * speed));
+                                Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                            }
+                        }
+                        if(s.getName().equalsIgnoreCase("SAT3")){
+                            Location.distanceBetween(s.getLatLng().latitude, s.getLatLng().longitude-0.1*speed, target.latitude, target.longitude, results);
+                            if(results[0] <= 2000000){
+                                mMap.addMarker(new MarkerOptions().title("Votre position").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                                h.removeCallbacks(r);
+                            }else {
+                                Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                                s.setLatLng(new LatLng(s.getLatLng().latitude, s.getLatLng().longitude  - 0.1 * speed));
+                                Log.w("Satellite", "Position : " + s.getLatLng().toString());
+                            }
+                        }
+
                     }
                     updateMap();
                 }
 
-                update();
+
             }
         };
 
-        init();
+        //init();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        init();
 
+
+
+    }
+
+    private void etape3() {
+        line.remove();
+        instructions.setText("L’ensemble des points de l’espace situé à la distance du satellite est la sphère centrée sur le satellite." +
+                " On sait donc que le récepteur est situé sur cette sphère.");
+        LatLng latlngS = new LatLng(s.getLatLng().latitude+10, s.getLatLng().longitude);
+        LatLng latlngU = new LatLng(target.latitude, target.longitude);
+        float[] results = new float[3];
+        Location.distanceBetween(latlngS.latitude, latlngS.longitude, target.latitude, target.longitude, results);
+        double dist = results[0];
+        mMap.addCircle(new CircleOptions().center(latlngS).strokeWidth(5).fillColor(Color.TRANSPARENT).radius(dist).strokeColor(Color.RED));
+        etape = 4;
+    }
+    public void etape4(){
+        instructions.setText("Cette donnée ne suffit pas à connaître précisément la position du récepteur. " +
+                "Le récepteur capte donc le signal d’un deuxième satellite et effectue la même opération");
+        ajoutMarker(s1);
+        LatLng latlngS = new LatLng(s1.getLatLng().latitude, s1.getLatLng().longitude);
+        float[] results = new float[3];
+        Location.distanceBetween(latlngS.latitude, latlngS.longitude, target.latitude, target.longitude, results);
+        double dist = results[0];
+        mMap.addCircle(new CircleOptions().center(latlngS).strokeWidth(5).fillColor(Color.TRANSPARENT).radius(dist).strokeColor(Color.RED));
+        etape = 5;
+    }
+
+    public void etape5(){
+
+        LatLng latlngS = s.getLatLng();
+        LatLng latlngS1 = s1.getLatLng();
+
+        float[] results = new float[3];
+        Location.distanceBetween(latlngS.latitude, latlngS.longitude, latlngS1.latitude, latlngS1.longitude, results);
+        double middle = results[0];
+
+        LatLng latLngMiddle = new LatLng(0+middle,-15);
+        etape = 6;
+
+
+
+
+    }
+
+    public void etape6(){
+        instructions.setText("Puis un troisième sattelite");
+        ajoutMarker(s2);
+        LatLng latlngS = new LatLng(s2.getLatLng().latitude, s2.getLatLng().longitude);
+        float[] results = new float[3];
+        Location.distanceBetween(latlngS.latitude, latlngS.longitude, target.latitude, target.longitude, results);
+        double dist = results[0];
+        mMap.addCircle(new CircleOptions().center(latlngS).strokeWidth(5).fillColor(Color.TRANSPARENT).radius(dist).strokeColor(Color.RED));
+        etape = 7;
+
+    }
+
+    private void etape7(){
+        instructions.setText("On connait maintenant votre position de longitude : "+target.longitude+" et de latitude : "+target.latitude);
+        m.remove();
+        mMap.addMarker(new MarkerOptions().title("Votre potisition").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
+                .icon(BitmapDescriptorFactory.defaultMarker()));
+        etape = 8;
+    }
+
+    private void etape2() {
+
+        LatLng latlngS = new LatLng(s.getLatLng().latitude+5, s.getLatLng().longitude);
+        LatLng latlngU = new LatLng(target.latitude, target.longitude);
+        line = mMap.addPolyline(new PolylineOptions()
+                .add(latlngS, latlngU)
+                .width(5)
+                .color(Color.RED));
+        instructions.setText("Le récepteur mesure le temps mis par le " +
+                "signal émis par le satellite pour parcourir la distance " +
+                "qui le sépare du satellite.");
+
+        etape = 3;
+
+    }
+
+    public void etape1(){
+        m = mMap.addMarker(new MarkerOptions().title("Récepteur").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.antenne)));
+        /*mMap.addMarker(new MarkerOptions().title("Récepteur").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.antenne)));*/
+        h1.postDelayed(r1,0);
+        etape = 1;
+
+    }
+
+    public void etape1bis(){
+        instructions.setText("Un signal est émis par un premier sattelite");
+        ajoutMarker(s);
+
+        suivant.setVisibility(View.VISIBLE);
+        etape = 2;
+    }
+
+    private void ajoutMarker(Satellite sat) {
+        mMap.addMarker(new MarkerOptions().title(sat.getName()).snippet("Latitude : " + sat.getLatLng().latitude + " Longitude : " + sat.getLatLng().longitude)
+                .position(sat.getLatLng()).icon(BitmapDescriptorFactory.fromResource(R.drawable.satellite)));
     }
 
     /**
@@ -118,7 +288,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        update();
+        etape1();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -130,12 +300,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 float[] results = new float[3];
                 for(Satellite s : satelliteList) {
-                    LatLng k = new LatLng(s.getLatLng().latitude+7, s.getLatLng().longitude);
+                    LatLng k = new LatLng(s.getLatLng().latitude, s.getLatLng().longitude);
                     mMap.addMarker(new MarkerOptions().title(s.getName()).snippet("Latitude : " + s.getLatLng().latitude + " Longitude : " + s.getLatLng().longitude)
                             .position(s.getLatLng()).icon(BitmapDescriptorFactory.fromResource(R.drawable.satellite)));
-                    mMap.addCircle(new CircleOptions().center(k).strokeWidth(7).fillColor(Color.TRANSPARENT).radius(2500000).strokeColor(Color.RED));
-                    Location.distanceBetween(s.getLatLng().latitude+7+0.5*speed, s.getLatLng().longitude, target.latitude, target.longitude, results);
-                    if(results[0] < 2500000){
+                    mMap.addCircle(new CircleOptions().center(k).strokeWidth(5).fillColor(Color.TRANSPARENT).radius(2000000).strokeColor(Color.RED));
+                    Location.distanceBetween(s.getLatLng().latitude+0.1*speed, s.getLatLng().longitude, target.latitude, target.longitude, results);
+                    if(results[0] <= 2000000){
                         mMap.addMarker(new MarkerOptions().title("Votre position").snippet("Latitude : " + target.latitude + " Longitude : " + target.longitude).position(target)
                                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
                     }
@@ -144,13 +314,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void update(){
-        h.postDelayed(r, 500);
-    }
+
 
     public void init(){
-        Satellite s = new Satellite("SAT1", new LatLng(-47, -3));
-        satelliteList.add(s);
+        s = new Satellite("SAT1", new LatLng(10, 0));
+        s1 = new Satellite("SAT2", new LatLng(40, -30));
+        s2 = new Satellite("SAT3", new LatLng(60, 10));
     }
 
 
